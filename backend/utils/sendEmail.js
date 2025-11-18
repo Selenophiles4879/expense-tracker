69251eb17dd8bb06630a0955f3b4c67b7a180bc8
Shreyansh Tiwari <shreyanshtiwari12062003@gmail.com>
2025-11-18 23:04:06 +0530

    sendEmail.js

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -7,8 +7,8 @@ const sendEmail = async (options) => {
   const transporter = nodemailer.createTransport({
     service: "Gmail",
     auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS,
     },
   });
 
