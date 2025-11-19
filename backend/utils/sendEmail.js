04c1c69014035ad41c401879ea22c5be9016fa5a
Shreyansh Tiwari <shreyanshtiwari12062003@gmail.com>
2025-11-19 08:38:43 +0530

    sendEmail.js

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -10,6 +10,9 @@ const sendEmail = async (options) => {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
       },
+      tls: {
+        rejectUnauthorized: false,
+      }
     });
 
     // 2. Email options
