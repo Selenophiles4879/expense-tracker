5cd7a3a7a7bcf4c5e4dc4d966c5ed70e0d62c277
Shreyansh Tiwari <shreyanshtiwari12062003@gmail.com>
2025-11-19 12:29:19 +0530

    url.js

--- frontend/src/utils/url.js
+++ frontend/src/utils/url.js
@@ -1,1 +1,2 @@
-export const BASE_URL = "https://expense-tracker-rds0.onrender.com/api/v1";
+//export const BASE_URL = "https://expense-tracker-rds0.onrender.com/api/v1";
+export const BASE_URL = import.meta.env.VITE_BASE_URL;
