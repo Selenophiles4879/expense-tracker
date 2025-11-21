bf3c00ae69f8e716866227e7fecd1f95535b8ee6
Shreyansh Tiwari <shreyanshtiwari12062003@gmail.com>
2025-11-21 23:15:21 +0530

    app.js

--- backend/app.js
+++ backend/app.js
@@ -19,7 +19,8 @@ mongoose
 
 // Allow your specific Netlify frontend to access the backend
 const corsOptions = {
-  origin: 'https://expensetracker4879.netlify.app', // <--- NEW // Your actual Frontend URL
+  //origin: 'https://expensetracker4879.netlify.app', // <--- NEW // Your actual Frontend URL
+  origin: 'https://expensetracker48.netlify.app',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true // Allow cookies if you use them
