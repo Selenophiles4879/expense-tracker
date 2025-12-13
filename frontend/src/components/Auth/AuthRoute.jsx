226e499f2300742e966611740a86e4c3fa8b1274
Shreyansh Tiwari <shreyanshtiwari12062003@gmail.com>
2025-12-14 01:58:12 +0530

    AuthRoute.jsx

--- frontend/src/components/Auth/AuthRoute.jsx
+++ frontend/src/components/Auth/AuthRoute.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
 import { getUserFromStorage } from "../../utils/getUserFromStorage";
+import { useSelector } from "react-redux";
 
 const AuthRoute = ({ children }) => {
-  const token = getUserFromStorage();
+  //const token = getUserFromStorage();
+  const user = useSelector((state) => state?.auth?.user);
 
-  if (!token) {
+  if (!user) {
     return <Navigate to="/login" replace />;
   }
 
