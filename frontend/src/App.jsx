9558bbd8fd09d59608f3ee91fc6c9611f3331e3c
Shreyansh Tiwari <shreyanshtiwari12062003@gmail.com>
2025-12-14 01:12:14 +0530

    App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,17 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { logoutAction } from "./redux/slice/authSlice";
+
+// Public Components
 import HeroSection from "./components/Home/HomePage";
 import PublicNavbar from "./components/NavBar/PublicNavBar";
 import LoginForm from "./components/Users/Login";
-import { useSelector } from "react-redux";
 import RegistrationForm from "./components/Users/Register";
+import ForgotPassword from "./components/Users/ForgotPassword";
+import ResetPassword from "./components/Users/ResetPassword";
+
+// Private Components
 import PrivateNavbar from "./components/NavBar/PrivateNavBar";
 import AddCategory from "./components/Category/AddCategory";
 import CategoriesList from "./components/Category/CategoriesList";
@@ -12,22 +20,24 @@ import TransactionForm from "./components/Transactions/TransactionForm";
 import Dashboard from "./components/Users/Dashboard";
 import UserProfile from "./components/Users/UserProfile";
 import AuthRoute from "./components/Auth/AuthRoute";
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { logoutAction } from "./redux/slice/authSlice";
 
-
-import ForgotPassword from "./components/Users/ForgotPassword"; // <-- ADD
-import ResetPassword from "./components/Users/ResetPassword"; // <-- ADD
-
-function App() {
+/**
+ * INTERNAL APP CONTENT
+ * (Needed so we can safely use useNavigate)
+ */
+function AppContent() {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
+  // 🔐 Redux auth state (SINGLE SOURCE OF TRUTH)
+  const user = useSelector((state) => state.auth.user);
+
+  // 🔄 MULTI-TAB LOGOUT SYNC (MANDATORY FIX)
   useEffect(() => {
     const handleStorageChange = (event) => {
       if (event.key === "userInfo" && !event.newValue) {
         dispatch(logoutAction());
-        window.location.href = "/login";
+        navigate("/login", { replace: true });
       }
     };
 
@@ -36,19 +46,22 @@ function App() {
     return () => {
       window.removeEventListener("storage", handleStorageChange);
     };
-  }, [dispatch]);
-
-  const user = useSelector((state) => state?.auth?.user);
+  }, [dispatch, navigate]);
 
   return (
-    <BrowserRouter>
-      {/* Navbar */}
-
+    <>
+      {/* 🔁 AUTH-BASED NAVBAR */}
       {user ? <PrivateNavbar /> : <PublicNavbar />}
+
       <Routes>
+        {/* PUBLIC ROUTES */}
         <Route path="/" element={<HeroSection />} />
         <Route path="/login" element={<LoginForm />} />
         <Route path="/register" element={<RegistrationForm />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/users/reset-password/:token" element={<ResetPassword />} />
+
+        {/* PRIVATE ROUTES */}
         <Route
           path="/add-category"
           element={
@@ -97,12 +110,18 @@ function App() {
             </AuthRoute>
           }
         />
-
-        {/* --- ADD THESE NEW ROUTES --- */}
-        <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="/users/reset-password/:token" element={<ResetPassword />} />
-
       </Routes>
+    </>
+  );
+}
+
+/**
+ * ROOT APP
+ */
+function App() {
+  return (
+    <BrowserRouter>
+      <AppContent />
     </BrowserRouter>
   );
 }
