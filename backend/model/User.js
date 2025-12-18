80f29041622b9adbefe36f1311acbac0e9afe402
Shreyansh Tiwari <shreyanshtiwari12062003@gmail.com>
2025-12-18 19:24:32 +0530

    User.js

--- backend/model/User.js
+++ backend/model/User.js
@@ -17,9 +17,16 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    // Email verification
+   isEmailVerified: {
+     type: Boolean,
+     default: false,
+   },
     // --- ADD THESE NEW FIELDS ---
     passwordResetToken: String,
     passwordResetExpires: Date,
+    emailVerificationToken: String,
+    emailVerificationExpires: Date,
   },
   {
     timestamps: true,
@@ -45,5 +52,20 @@ userSchema.methods.createPasswordResetToken = function () {
   // 4. Return the *un-hashed* token
   return resetToken;
 };
+
+//Email verification
+userSchema.methods.createEmailVerificationToken = function () {
+  const token = crypto.randomBytes(32).toString("hex");
+
+  this.emailVerificationToken = crypto
+    .createHash("sha256")
+    .update(token)
+    .digest("hex");
+
+  this.emailVerificationExpires = Date.now() + 15 * 60 * 1000;
+
+  return token;
+};
+
 
 module.exports = mongoose.model("User", userSchema);
