d736cf88ca1371535a7dadde1a90bbe397af67bd
Shreyansh Tiwari <shreyanshtiwari12062003@gmail.com>
2026-09-05 12:37:06 +0530

    Update Transaction.js

--- backend/model/Transaction.js
+++ backend/model/Transaction.js
@@ -1,4 +1,3 @@
-
 const mongoose = require("mongoose");
 
 const transactionSchema = new mongoose.Schema(
@@ -35,6 +34,22 @@ const transactionSchema = new mongoose.Schema(
       type: String,
       required: false,
     },
+
+    // Items are used for expense transactions
+    items: [
+      {
+        name: {
+          type: String,
+          required: true,
+          trim: true,
+        },
+        price: {
+          type: Number,
+          required: true,
+          min: 0,
+        },
+      },
+    ],
   },
   {
     timestamps: true,
