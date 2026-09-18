8074d964c1df23b0d89faaa3cb6545e64d0512a1
Shreyansh Tiwari <shreyanshtiwari12062003@gmail.com>
2026-09-18 20:34:54 +0530

    Update error message for user not found

--- backend/controllers/usersCtrl.js
+++ backend/controllers/usersCtrl.js
@@ -336,7 +336,7 @@ const usersController = {
     if (!user) {
       res.status(404);
       throw new Error(
-        "User not found. Please check your email."
+        "User not found. Please register yourself."
       );
     }
 
