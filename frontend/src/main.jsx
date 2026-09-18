4bfa391d78be0e7760e4fce22560f2b3c526702f
Shreyansh Tiwari <shreyanshtiwari12062003@gmail.com>
2026-09-18 08:22:23 +0530

    Add service worker registration
    Register service worker for PWA support.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,10 +3,16 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { registerSW } from 'virtual:pwa-register';
 
 import App from "./App.jsx";
 import "./index.css";
 import { store } from "./redux/store/store.js";
+
+registerSW({
+  immediate: true
+})
+
 //! instance of react query
 const client = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")).render(
