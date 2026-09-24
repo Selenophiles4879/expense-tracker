988a98645b303549047d3a14c8824c787440abed
Shreyansh Tiwari <shreyanshtiwari12062003@gmail.com>
2026-09-24 22:09:15 +0530

    Update About.jsx

--- frontend/src/components/About/About.jsx
+++ frontend/src/components/About/About.jsx
@@ -9,7 +9,7 @@ const About = () => {
           {/* Photo */}
           <div className="flex justify-center pt-10">
             <img
-              src="https://drive.google.com/uc?export=view&id=1xnum2OjGLKZ61K2WTvTyEbwx3m2S1wc9"
+              src="https://drive.google.com/thumbnail?id=1xnum2OjGLKZ61K2WTvTyEbwx3m2S1wc9&sz=w1000"
               alt="Profile"
               className="h-40 w-40 rounded-full object-cover border-4 border-white shadow-md dark:border-gray-800"
             />
